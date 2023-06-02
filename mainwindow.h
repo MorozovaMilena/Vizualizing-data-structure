beb7f2ec308515373e4b6c46d6d732a6a8bc7fa9
MorozovaMilena <84088568+MorozovaMilena@users.noreply.github.com>
2023-06-02 22:34:39 +0300

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,56 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QDebug>
+#include <QWheelEvent>
+#include <QGridLayout>
+#include <QToolButton>
+#include <QSlider>
+#include <QLabel>
+#include <QComboBox>
+#include <QLineEdit>
+#include <cmath>
+#include <QRegExpValidator>
+#include <QValidator>
+#include <string>
+#include <sstream>
+#include <QGraphicsTextItem>
+#include <QMessageBox>
+#include <QPushButton>
+#include "VertexList.h"
+#include "AVLTree.h"
+
+
+class MainWindow : public QMainWindow {
+Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget * parent = nullptr);
+    ~MainWindow() override = default;
+    void commandChanged(const QString &s);
+    void zoomer(int val);
+    AVLTree * avl = new AVLTree;
+    enum Tree {AVL};
+    Tree curTreeType = AVL;
+    void clearAll();
+private:
+    QGraphicsView * view{};
+    QGridLayout * grid{};
+    double scaleCF;
+    QLineEdit * insertLineEdit;
+    VertexList * vertex;
+    QLabel * elementsCountLabel;
+    QSlider * zoom;
+    void vertexListAdd();
+    void vertexListRemove();
+    void vertexRemoved(int key);
+    void drawVertex(int sizeX, int key, int x, int y, bool red = false);
+    void drawTree();
+    void drawChildrens(int sizeX, Node * p, int px, int py);
+    QGraphicsScene * scene;
+};
+
+#endif // MAINWINDOW_H
